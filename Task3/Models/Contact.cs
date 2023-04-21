2a2a266be2142ec9138ed88c761440dc4c05e684
senia1 <oks314@gmail.com>
2023-04-21 18:44:15 +0300

    Add project files.

---
+++ Task3/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿namespace Task3.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+    }
+}
