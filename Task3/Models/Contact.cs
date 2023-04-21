2dd94edb8b2efd347f4428c1fedb3afe5b37ac9a
senia1 <oks314@gmail.com>
2023-04-21 23:20:56 +0300

    added check fields of form

--- Task3/Models/Contact.cs
+++ Task3/Models/Contact.cs
@@ -1,15 +1,26 @@
-﻿namespace Task3.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task3.Models
 {
     public class Contact
     {
+        [Display(Name="Введите имя")]
+        [Required(ErrorMessage ="Вам нужно ввести имя")]
         public string Name { get; set; }
 
+        [Display(Name = "Введите фамилию")]
+        [Required(ErrorMessage = "Вам нужно ввести фамилию")]
         public string Surname { get; set; }
 
+        [Display(Name = "Введите возраст")]
+        [Required(ErrorMessage = "Вам нужно ввести возраст")]
         public int Age { get; set; }
-
+        [Display(Name = "Введите электронную почту")]
+        [Required(ErrorMessage = "Вам нужно ввести электронную почту")]
         public string Email { get; set; }
-
+        [Display(Name = "Введите ваше сообщение")]
+        [StringLength(30, ErrorMessage="Текст не менее 30 символов")]
+        [Required(ErrorMessage = "Вам нужно ввести сообщение")]
         public string Message { get; set; }
     }
 }
